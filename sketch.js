68d78c1b69c98ac4857b96fd553cc950a117c9de
andreas-dora <andreas.dora@icloud.com>
2019-11-05 08:22:50 +0100

    iframe hinzugefügt

--- sketch.js
+++ sketch.js
@@ -9,9 +9,8 @@ function CustomAlert(){
         dialogBox.style.left = (winW/2) - ( 300 * .5)+"px";
         dialogBox.style.top = "100px";
         dialogBox.style.display = "block";
-        document.getElementById('dialogBoxHead').innerHTML = "FEHLER: Bitte trag deinen Vor- und Nachnamen ein.";
-        document.getElementById('dialogBoxBody').innerHTML = dialog;
-        document.getElementById('dialogBoxFoot').innerHTML = '<button id="ok" onclick="Alert.ok()">OK</button>';
+    //    document.getElementById('dialogBoxHead').innerHTML = "FEHLER: Bitte trag deinen Vor- und Nachnamen ein.";
+   //     document.getElementById('dialogBoxFoot').innerHTML = '<button id="ok" onclick="Alert.ok()">OK</button>';
 
     }
     this.ok = function(){
