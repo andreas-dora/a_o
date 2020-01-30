7d756c33e270ad25a8bb95e599bf7fd040086840
andreas-dora <andreas.dora@icloud.com>
2020-01-30 22:57:04 +0100

    INfos Rechnungsstellung

---
+++ new/rechnung/sketch.js
@@ -0,0 +1,41 @@
+window.onscroll = function() {stickFunction()};
+var nav = document.getElementById("myNav");
+var sticky = nav.offsetTop;
+function stickFunction(){
+  if(window.pageYOffset >=sticky){
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+// }
+// var gkModal = document.getElementById("myGkModal");
+// var sDr = document.getElementById("sDrinks");
+// var gkGr = document.getElementById("GKgr");
+// var captionText=document.getElementById("caption");
+// var lau = document.getElementById("lau");
+// var lauModal = document.getElementById("lauModal");
+// var lauGr = document.getElementById("lauGr");
+
+// sDr.onclick = function(){
+//   gkModal.style.display = "block";
+//   gkGr.scr = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// lau.onclick = function(){
+//   lauModal.style.display = "block";
+//   lauGr.scr = this.src;
+//   // captionText.innerHTML = this.alt;
+// }
+
+
+// var span = document.getElementsByClassName("close")[0];
+// span.onclick =function(){
+//   gkModal.style.display = "none";
+// }
+
+// var span = document.getElementsByClassName("close")[1];
+// span.onclick =function(){
+//   lauModal.style.display = "none";
+// }
