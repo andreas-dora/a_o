72f89f5f59cc802dc5bf28a76d0a8804af2e7f02
andreas-dora <andreas.dora@icloud.com>
2020-02-01 05:36:59 +0100

    ohne gl

---
+++ new/tolate/sketch.js
@@ -0,0 +1,11 @@
+
+var i = 0;
+var txt = 'Uh, äh, sorry, sorry, aber Du bist viel zu spät. die Gästeliste ist schon längst geschlossen. MAch dir keinen Kopf: Die nächste Party kommt bestimmt. Vielleicht schaffst Du es dann. '; /* The text */
+var speed = 90; 
+function write() {
+  if (i < txt.length) {
+    document.getElementById("robo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(write, speed);
+  }
+}
