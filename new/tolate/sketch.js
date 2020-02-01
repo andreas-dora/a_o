1b67e7a9fc1365a249e9c215c0a106ca3f27b73f
andreas-dora <andreas.dora@icloud.com>
2020-02-01 12:24:25 +0100

    nice gl ende

--- new/tolate/sketch.js
+++ new/tolate/sketch.js
@@ -1,7 +1,7 @@
 
 var i = 0;
-var txt = 'Uh, äh, sorry, sorry, aber Du bist viel zu spät. die Gästeliste ist schon längst geschlossen. MAch dir keinen Kopf: Die nächste Party kommt bestimmt. Vielleicht schaffst Du es dann. '; /* The text */
-var speed = 90; 
+var txt = 'Uh, äh, sorry, sorry, aber Du bist viel zu spät. die Gästeliste ist schon längst geschlossen. MAch dir keinen Kopf: Die nächste Party kommt bestimmt. Vielleicht schaffst Du es dann. Nur mut...'; /* The text */
+var speed = 70; 
 function write() {
   if (i < txt.length) {
     document.getElementById("robo").innerHTML += txt.charAt(i);
